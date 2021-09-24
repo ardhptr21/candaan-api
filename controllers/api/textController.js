ffc48d2104f7d9e08a20f571c0070c25a269f11a
ardhptr21 <ardhiputrapradana21@gmail.com>
2021-09-24 21:44:01 +0700

    added new information about status on routes and add engines node version

--- controllers/api/textController.js
+++ controllers/api/textController.js
@@ -2,6 +2,7 @@ const text = require("../../data/api/text.json");
 
 module.exports.getAllTextJokes = (req, res) => {
   res.status(200).json({
+    status: 200,
     total: text.length,
     route: req.originalUrl,
     method: req.method,
@@ -12,6 +13,7 @@ module.exports.getAllTextJokes = (req, res) => {
 module.exports.randomTextJoke = (req, res) => {
   const selectedIndex = Math.floor(Math.random() * text.length);
   res.status(200).json({
+    status: 200,
     route: req.originalUrl,
     method: req.method,
     data: text[selectedIndex],
